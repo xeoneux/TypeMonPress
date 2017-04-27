30cd2796ece175ea5d82a38fc62231bfddd69578
Aayush Kapoor <xeoneux@icloud.com>
2017-04-27 15:10:22 +0530

    Run Demo Express Server

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
-import express from "express"
+import * as express from "express"
+
+import { Application } from "express"
 
 class TypeMonPress {
-    public app
+    public app: Application
 
     constructor() {
         this.app = express()
     }
+
+    public bootstrap() {
+        this.app.listen(1234)
+    }
 }
+
+const app = new TypeMonPress()
+
+app.bootstrap()
