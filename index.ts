ed19fb9b1bdc02ccdff379f0413eb33fe17c1429
Aayush Kapoor <xeoneux@icloud.com>
2017-05-03 14:34:34 +0530

    Configure Express Application

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
-import * as express from "express"
-
 import { Application } from "express"
 
+import { json, urlencoded } from "body-parser"
+import * as compression from "compression"
+import * as cookieParser from "cookie-parser"
+import * as cors from "cors"
+import * as express from "express"
+import * as helmet from "helmet"
 import config from "./config/config"
 
 class App {
@@ -9,10 +13,22 @@ class App {
 
     constructor(config: object) {
         this.app = express()
+        this.configure()
     }
 
     public run() {
         this.app.listen(1234)
+    }
+
+    private configure() {
+        const app = this.app
+
+        app.use(compression())
+        app.use(cookieParser())
+        app.use(cors())
+        app.use(json())
+        app.use(helmet())
+        app.use(urlencoded({ extended: true }))
     }
 }
 
