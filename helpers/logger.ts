be6978c3289f29625ec6879fb9df0897de3e803c
Aayush Kapoor <xeoneux@icloud.com>
2017-05-27 18:06:58 +0530

    Configure Winston Logger

--- helpers/logger.ts
+++ helpers/logger.ts
@@ -1,10 +1,24 @@
-import { Logger, transports } from "winston"
+import winston = require("winston")
 
-export const logger = new Logger({
+export const logger = new winston.Logger({
+    exitOnError: false,
     transports: [
-        new transports.Console({
-            colorize: true,
+        new winston.transports.DailyRotateFile({
+            colorize: false,
+            datePattern: "yy-MM-dd.",
+            filename: "../bin/log",
+            handleExceptions: true,
             json: true,
+            level: "info",
+            maxFiles: 5,
+            maxsize: 5242880, // 5 MB
+            prepend: true,
+        }),
+        new winston.transports.Console({
+            colorize: true,
+            handleExceptions: true,
+            json: false,
+            level: "debug",
         }),
     ],
 })
--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { Application } from "express"
-
 import bluebird = require("bluebird")
 import bodyParser = require("body-parser")
 import compression = require("compression")
@@ -9,56 +7,44 @@ import cors = require("cors")
 import express = require("express")
 import helmet = require("helmet")
 import mongoose = require("mongoose")
+import morgan = require("morgan")
 import session = require("express-session")
 import validator = require("express-validator")
 
 import config from "./config"
 import router from "./routes"
 
-class App {
-    private app: Application
+// Database
+mongoose.Promise = bluebird
+mongoose.connect(config.mongo.host, { server: { socketOptions: { keepAlive: 1 } } })
+mongoose.connection.on("error", () => {
+    throw new Error(`Unable to Connect to Database: ${config.mongo.host} on Port ${config.mongo.port}`)
+})
 
-    constructor() {
-        this.app = express()
-        this.configure()
-        this.database()
-    }
+// Configure
+const app = express()
 
-    public run() {
-        this.app.listen(1234)
-    }
-
-    private configure() {
-        const app = this.app
-
-        app.use(cors())
-        app.use(helmet())
-        app.use(compression())
-        app.use(cookieParser())
-        app.use(bodyParser.json())
-        app.use(bodyParser.urlencoded({ extended: true }))
-        app.use(validator())
-
-        const MongoStore = connectMongo(session)
-        app.use(session({
-            resave: false,
-            saveUninitialized: true,
-            secret: "secret",
-            store: new MongoStore({
-                mongooseConnection: mongoose.connection,
-            }),
-        }))
-
-        app.use(router)
-    }
-
-    private database() {
-        mongoose.Promise = bluebird
-        mongoose.connect(config.mongo.host, { server: { socketOptions: { keepAlive: 1 } } })
-        mongoose.connection.on("error", () => {
-            throw new Error(`Unable to Connect to Database: ${config.mongo.host} on Port ${config.mongo.port}`)
-        })
-    }
+if (config.node.env === "development") {
+    app.use(morgan("dev"))
 }
 
-new App().run()
+app.use(cors())
+app.use(helmet())
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(validator())
+
+const MongoStore = connectMongo(session)
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "secret",
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+    }),
+}))
+app.use(router)
+
+app.listen(1234)
