7aa59f8a9dda934e3b2ce768ef35fae6d3b8c75d
Aayush Kapoor <xeoneux@icloud.com>
2017-08-16 21:56:27 +0530

    Add logger environment transports

--- helpers/logger.ts
+++ helpers/logger.ts
@@ -1,30 +1,39 @@
+import { mongo, node } from "../config";
 import { existsSync, mkdirSync } from "fs";
-import winston = require("winston");
+import { config, Logger, addColors, setLevels, transports } from "winston";
+
+setLevels(config.syslog.levels);
+addColors(config.syslog.colors);
 
 if (!existsSync("logs")) mkdirSync("logs");
 
-winston.setLevels(winston.config.syslog.levels);
-winston.addColors(winston.config.syslog.colors);
-
-const logger = new winston.Logger({
-  exitOnError: false,
-  transports: [
-    new winston.transports.File({
-      colorize: false,
-      filename: "./logs/log",
-      handleExceptions: true,
-      json: true,
-      level: "info",
-      maxFiles: 5,
-      maxsize: 1024 * 1024 * 10 // 10 MB
-    }),
-    new winston.transports.Console({
-      colorize: true,
-      handleExceptions: true,
-      json: false,
-      level: "debug"
-    })
-  ]
+const consoleTransport = new transports.Console({
+  colorize: true,
+  handleExceptions: true,
+  json: false,
+  level: "debug"
 });
+
+const fileTransport = new transports.File({
+  colorize: false,
+  filename: "logs/log",
+  handleExceptions: true,
+  json: true,
+  level: "info",
+  maxFiles: 5,
+  maxsize: 1024 * 1024 * 10 // 10 MB
+});
+
+let logger = new Logger();
+
+if (node.env === "test")
+  logger = new Logger({ exitOnError: false, transports: [fileTransport] });
+if (node.env === "development")
+  logger = new Logger({ exitOnError: false, transports: [consoleTransport] });
+if (node.env === "production")
+  logger = new Logger({
+    exitOnError: false,
+    transports: [consoleTransport, fileTransport]
+  });
 
 export default logger;
